c4ed23e86cdaf01b0b0eba83f03462605d8a6d31
anuradha.manolla <anuradha.manolla@oracle.com>
2022-04-10 18:53:01 +0100

    updated userdata

--- es_userdata.sh
+++ es_userdata.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Install Filebeat"
-wget -O /root/ https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.10.0-linux-x86_64.tar.gz
+wget -O /root/filebeat-7.10.0-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.10.0-linux-x86_64.tar.gz
 
-tar -zxvf filebeat-7.10.0-linux-x86_64.tar.gz
+tar -zxvf /root/filebeat-7.10.0-linux-x86_64.tar.gz -C /root/
 cd /root/filebeat-7.10.0-linux-x86_64
-tar -zxvf /root/filebeat-oss-7.12.1-linux-x86_64.tar.gz -C /root/
+#tar -zxvf /root/filebeat-oss-7.12.1-linux-x86_64.tar.gz -C /root/
 sed -i 's/localhost:9200/${es_cluster}:443/g' /root/filebeat-7.12.1-linux-x86_64/filebeat.yml
 sed -i 's/#protocol: \"https\"/protocol: \"https\"/g' /root/filebeat-7.12.1-linux-x86_64/filebeat.yml
 systemctl start jenkins
@@ -16,5 +16,26 @@ echo "setup.template.enabled: false" >> /root/filebeat-7.12.1-linux-x86_64/fileb
 
 echo "successgully installed Filebeat"
 
+echo "Intall Packetbeat"
+wget -O /root/packetbeat-oss-7.12.1-linux-x86_64.tar.gz https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-oss-7.12.1-linux-x86_64.tar.gz
+tar -zxvf /root/packetbeat-oss-7.12.1-linux-x86_64.tar.gz
+Update packetbeat.yaml 
+setup.ilm.enabled: false
+setup.pack.security.enabled: false
+setup.xpack.graph.enabled: false
+setup.xpack.watcher.enabled: false
+setup.xpack.monitoring.enabled: false
+setup.xpack.reporting.enabled: false
+
+output.elasticsearch:
+  # Array of hosts to connect to.
+  hosts: ["search-test-xmuccn2ie224hszu56noofp6ti.us-east-1.es.amazonaws.com:443"]
+
+  # Protocol - either `http` (default) or `https`.
+  protocol: "https"
+  #
+start service
+./packetbeat -e
 
 
+./packetbeat -e -c packetbeat.yml  -I archivo.pcap -d "publish"
